bed39c922ec3d5a87bafc2a68b7e33a6201812ad
Jakub Kadlčík <frostyx@email.cz>
2015-11-06 16:32:55 +0100

    Add unit tests

---
+++ tests/test_system.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestLang(unittest.TestCase):
+
+    def test_some_stupid_thing(self):
+        self.assertEqual(True, True)
+
+    def test_another_stupid_thing(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
